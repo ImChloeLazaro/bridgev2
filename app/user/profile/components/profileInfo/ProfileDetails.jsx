a995038fb1e9f03dbede6e7dbb53bbb63cd0950e
ImChloeLazaro <ckhemskie@gmail.com>
2024-01-30 13:31:42 +1100

    Revert "added remaining profile components"
    This reverts commit 6b0edf5755c453cf4e0ce4483de90f09bf663df8.

--- app/user/profile/components/profileInfo/ProfileDetails.jsx
+++ app/user/profile/components/profileInfo/ProfileDetails.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { profileTabsAtom, selectedTabAtom } from "../../store/ProfileStore";
 
-import { Tabs, Tab, Divider, Avatar } from "@nextui-org/react";
+import { Tabs, Tab, Divider } from "@nextui-org/react";
 import { useAtom, useAtomValue } from "jotai";
 import { MdInfoOutline } from "react-icons/md";
 import LabelTag from "../../../../components/LabelTag";
@@ -37,97 +37,52 @@ const ProfileDetails = ({ data }) => {
         })}
       </Tabs>
       <Divider />
-      {/* // ### EMPLOYEE INFORMATION*/}
-      <div className="mt-2 mb-12 py-2 w-full">
-        <div className="flex justify-start items-center gap-2 mb-5">
-          <p className="font-bold text-lg">{"Employee Information"}</p>
+      <div className="my-2 py-2">
+        <div className="flex justify-start items-center gap-1">
+          <p className="font-bold text-lg">Employee Information</p>
           <MdInfoOutline />
         </div>
 
-        {/* // ### LIST */}
-        <div className="flex flex-col items-start gap-4 ">
-          {/* // ### ID Number */}
-          <div className="flex justify-start items-center gap-10 w-3/5">
-            <div className="flex-col w-1/2">
-              <p className="font-medium text-base">{"ID Number"}</p>
-              <p className="font-medium text-sm text-darkgrey-default">
-                {"This is your employee ID"}
-              </p>
+        {/* // ### IMMEDIATE HEAD */}
+        <div className="flex flex-col justify-start items-start gap-1">
+          <div className="flex justify-center items-center">
+            <div className="flex-col">
+              <p className="font-medium text-base">ID Number</p>
+              <p className="font-medium text-sm">This is your employee ID</p>
             </div>
             <p className="">{data.id}</p>
           </div>
 
           {/* // ### STATUS */}
-          <div className="flex justify-start items-center gap-10 w-3/5">
-            <p className="font-medium text-base w-1/2">{"Status"}</p>
+          <div className="flex justify-center items-center">
+            <p className="font-medium text-base">Status</p>
             <LabelTag text={"Active"} color={"green"} />
           </div>
 
           {/* // ### REGULARIZATION */}
-          <div className="flex justify-start items-center gap-10 w-3/5">
-            <div className="flex-col w-1/2">
-              <p className="font-medium text-base">{"Regularization"}</p>
-              <p className="font-medium text-sm text-darkgrey-default">
-                {"6 months after your start date"}
+          <div className="flex justify-center items-center">
+            <div className="flex-col">
+              <p className="font-medium text-base">Regularization</p>
+              <p className="font-medium text-sm">
+                6 months after your start date
               </p>
             </div>
-            <p className="">{"October 31, 2023"}</p>
+            <p className="">{"October 31,2023"}</p>
           </div>
 
           {/* // ### IMMEDIATE HEAD */}
-          <div className="flex justify-start items-center gap-10 w-3/5">
-            <p className="font-medium text-base w-1/2">{"Immediate Head"}</p>
-            <div className="flex items-center gap-2">
-              <Avatar
-                radius="full"
-                size="md"
-                src="/Madelyn Septimus.png"
-                alt="Supervisor Profile picture"
-              ></Avatar>
+          <div className="flex justify-center items-center">
+            <p className="font-medium text-base">Immediate Head</p>
+            <div className="flex-col">
               <p className="">{data.supervisor}</p>
             </div>
           </div>
         </div>
       </div>
-
-      {/* // ### PERSONAL INFORMATION*/}
-      <div className="mt-2 mb-12 py-2 w-full">
-        <div className="flex justify-start items-center gap-2 mb-5">
-          <p className="font-bold text-lg">{"Personal Information"}</p>
+      <div className="my-2 py-2">
+        <div className="flex justify-start items-center gap-1">
+          <p className="font-bold text-lg">{}</p>
           <MdInfoOutline />
-        </div>
-
-        {/* // ### LIST */}
-        <div className="flex flex-col items-start gap-4">
-          {/* // ### ADDRESS */}
-          <div className="flex justify-start items-center gap-10 w-3/5">
-            <div className="flex-col w-1/2">
-              <p className="font-medium text-base">{"Address"}</p>
-              <p className="font-medium text-sm text-darkgrey-default">
-                {"This is your current address"}
-              </p>
-            </div>
-            <p className="">{"105 Jerry Dove Drive, Florence, SC 29501"}</p>
-          </div>
-
-          {/* // ### CONTACT NUMBER */}
-          <div className="flex justify-start items-center gap-10 w-3/5">
-            <div className="flex-col w-1/2">
-              <p className="font-medium text-base">{"Contact Number"}</p>
-              <p className="font-medium text-sm text-darkgrey-default">
-                {"This is your current  contact number"}
-              </p>
-            </div>
-            <p className="">{"(765) 322-1399"}</p>
-          </div>
-
-          {/* // ### Birthday */}
-          <div className="flex justify-start items-center gap-10 w-3/5">
-            <div className="flex-col w-1/2">
-              <p className="font-medium text-base ">{"Birthday"}</p>
-            </div>
-            <p className="">{"October 4, 2001"}</p>
-          </div>
         </div>
       </div>
     </div>
