0c962a7caded8341a00b8ba197becfae41a1fd99
Chloe Kim Napiri Lazaro <95689120+ImChloeLazaro@users.noreply.github.com>
2024-07-01 08:20:28 +1000

    fix hr side bug

--- app/hr/page.jsx
+++ app/hr/page.jsx
@@ -5,7 +5,6 @@ import { useRoles } from "@/app/utils/roles";
 import { useAtomValue } from "jotai";
 import { userAtom } from "@/app/store/UserStore";
 const HumanResource = () => {
-  const auth = useAtomValue(authenticationAtom);
   const users = useAtomValue(userAtom).role; 
   const roles = useRoles(users);
   return <PlaceholderContent/>;
