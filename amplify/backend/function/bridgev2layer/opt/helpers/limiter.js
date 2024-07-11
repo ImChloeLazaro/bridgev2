1d8a22c222b60a387c447e2a3aeb55082f1c89b5
Chloe Kim Napiri Lazaro <95689120+ImChloeLazaro@users.noreply.github.com>
2024-07-11 13:52:49 +1000

    lambda

--- amplify/backend/function/bridgev2layer/opt/helpers/limiter.js
+++ amplify/backend/function/bridgev2layer/opt/helpers/limiter.js
@@ -2,7 +2,7 @@ const {rateLimit} = require('express-rate-limit')
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, 
+	limit: 2000, 
     message: 'Too many requests from this IP, please try again after 15 minutes'
 })
 
