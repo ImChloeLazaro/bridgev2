61056b3c68a791672369506e5606eacee133fbfa
Axel <johnaxelcortez21@gmail.com>
2024-07-25 17:03:16 +1000

    Fix bug when refresh at admin, tl, and hr

--- app/hr/layout.jsx
+++ app/hr/layout.jsx
@@ -1,13 +1,15 @@
 "use client";
 import { cn } from "@nextui-org/react";
-import { useAtomValue } from "jotai";
+import { useAtomValue, useSetAtom } from "jotai";
 import dynamic from "next/dynamic";
 import { usePathname } from "next/navigation";
 import OnboardingStatusAlert from "../components/OnboardingStatusAlert";
 import NavigationBar from "../navigation/components/NavigationBar";
 import { cmsPathsAtom } from "../navigation/store/NavSideBarStore";
 import { authenticationAtom } from "../store/AuthenticationStore";
-import { userAtom } from "../store/UserStore";
+import { fetchUserAtom, userAtom, userRegisterAtom } from "../store/UserStore";
+import { useEffect, useState } from "react";
+import { OnBoardingData } from "../onboarding/components/OnBoardingData";
 
 const SideBar = dynamic(() => import("../navigation/components/SideBar"), {
   ssr: false,
@@ -17,13 +19,37 @@ const HRLayout = ({ children }) => {
   const auth = useAtomValue(authenticationAtom);
   const user = useAtomValue(userAtom);
   const pathname = usePathname();
-
   const cmsPaths = useAtomValue(cmsPathsAtom);
   const collapseSidebar = cmsPaths.includes(pathname ?? "/");
 
-  if (auth.isAuthenticated) {
+  const userRegister = useSetAtom(userRegisterAtom);
+  const [isUserValid, setIsUserValid] = useState(false);
+  const fetchUser = useSetAtom(fetchUserAtom);
+  const onBoardData = OnBoardingData();
+  useEffect(() => {
+    if (auth && auth.sub) {
+      const checkRegistration = async () => {
+        try {
+          if (user && user.sub) {
+            setIsUserValid(true);
+          } else {
+            const result = await userRegister(onBoardData);
+            if (result && result?.result !== null) {
+              const fetch = await fetchUser();
+              setIsUserValid(true);
+            }
+          }
+        } catch (error) {
+          setIsUserValid(false);
+        }
+      };
+      checkRegistration();
+    }
+  }, [auth, user, onBoardData, fetchUser, userRegister]);
+
+  if (auth.isAuthenticated && isUserValid && user !== null) {
     return (
-      <div className="flex h-screen max-h-screen w-screen max-w-screen top-0">
+      <div className='flex h-screen max-h-screen w-screen max-w-screen top-0'>
         {/* <Suspense fallback={<Loading />}> */}
         <SideBar />
         {/* </Suspense> */}
@@ -33,11 +59,11 @@ const HRLayout = ({ children }) => {
             "flex flex-col w-full h-screen max-h-screen overflow-hidden"
           )}
         >
-          <div className="top-0">
+          <div className='top-0'>
             <OnboardingStatusAlert showAlert={user?.hasOnboardingData} />
             <NavigationBar />
           </div>
-          <div className="flex w-full h-screen max-h-screen overflow-x-hidden overflow-y-auto bg-background ">
+          <div className='flex w-full h-screen max-h-screen overflow-x-hidden overflow-y-auto bg-background '>
             {/* <Suspense fallback={<Loading />}> */}
             {children}
             {/* </Suspense> */}
