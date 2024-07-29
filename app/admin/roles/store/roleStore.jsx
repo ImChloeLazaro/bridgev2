a7c26614e99af0e3f10c866b56c684fa71da408b
cookiejarr1 <jarodlayugan@gmail.com>
2024-07-29 13:54:02 +1000

    user roles real time update after changing roles

--- app/admin/roles/store/roleStore.jsx
+++ app/admin/roles/store/roleStore.jsx
@@ -1,186 +1,63 @@
+import { userAtom } from "@/app/store/UserStore";
+import { restread, restupdate } from "@/app/utils/amplify-rest";
 import { atom } from "jotai";
 
-export const tabsAtom = atom([
-  {
-    id: "65eec076cbd659d0869a2bca",
-    sub: "d0229811-67cc-4fb8-915b-38d8029b85df",
-    email: "chloe.lazaro@aretex.com.au",
-    name: "Chloe Lazaro",
-    picture:
-      "https://lh3.googleusercontent.com/a/ACg8ocIxaddCAyXN_wh9WLB3DrR4tqUJOMWc31qXCUmmCtrLaA=s96-c",
-    hasOnboardingData: false,
-    role: [
-      {
-        _id: {
-          $oid: "65eec076cbd659d0869a2bcb",
-        },
-        name: "USER",
-        permissions: ["processor"],
-      },
-      {
-        name: "ADMIN",
-      },
-    ],
-    createdBy: {
-      $date: "2024-03-11T08:27:34.227Z",
-    },
-    __v: 0,
-  },
-  {
-    id: "65eec36d674c677b41e0a037",
-    sub: "3389ed50-e620-470a-80c2-e2d3f7a92c41",
-    email: "aretex.it@aretex.com.au",
-    name: "Aretex IT",
-    picture:
-      "https://lh3.googleusercontent.com/a/ACg8ocLL06xSNNZTwrHIOPa3MrHYkNeVWiSGbnuj17EnG5fy=s96-c",
-    hasOnboardingData: false,
-    role: [
-      {
-        _id: {
-          $oid: "65eec36d674c677b41e0a038",
-        },
-        name: "USER",
-        permissions: ["processor"],
-      },
-      {
-        name: "TL",
-      },
-    ],
-    createdBy: {
-      $date: "2024-03-11T08:40:13.626Z",
-    },
-    __v: 0,
-  },
-  {
-    id: "65ef007a05a0d27ae41ed001",
-    sub: "1857671a-fad8-4dcb-b7ae-171be5845fe5",
-    email: "reinier.silo@aretex.com.au",
-    name: "Reinier Silo",
-    picture:
-      "https://lh3.googleusercontent.com/a/ACg8ocJWVi5BJ-IIgBEntEnsDz9cg7VyDn9V6_KwqwMuReT6pfg=s96-c",
-    hasOnboardingData: true,
-    role: [
-      {
-        _id: {
-          $oid: "65ef007a05a0d27ae41ed002",
-        },
-        name: "USER",
-        permissions: ["processor"],
-      },
-    ],
-    createdBy: {
-      $date: "2024-03-11T13:00:42.952Z",
-    },
-    __v: 0,
-  },
-  {
-    id: "65ef80e94c6fd9ceffcf8e87",
-    sub: "a8dfd442-2977-499b-a917-a0e226c6c089",
-    email: "cyrus.layugan@aretex.com.au",
-    name: "Cyrus Layugan",
-    picture:
-      "https://lh3.googleusercontent.com/a/ACg8ocLpwxhx9lINMohpX7A8ewFwV4G9dKZ_oB2TK42jxweJ=s96-c",
-    hasOnboardingData: false,
-    role: [
-      {
-        _id: {
-          $oid: "65ef80e94c6fd9ceffcf8e88",
-        },
-        name: "USER",
-        permissions: ["processor"],
-      },
-    ],
-    createdBy: {
-      $date: "2024-03-11T22:08:41.540Z",
-    },
-    __v: 0,
-  },
-  {
-    id: "65f271e2b77b2abe952fc7fa",
-    sub: "5b0f4507-1edf-487f-ab5b-e2f99129a447",
-    email: "john.cortez@aretex.com.au",
-    name: "John Axel Cortez",
-    picture:
-      "https://lh3.googleusercontent.com/a/ACg8ocJrnrq8wb3u1UVxAQ9dbo4tbZUUKui0slL2gXKAZKR3=s96-c",
-    hasOnboardingData: false,
-    role: [
-      {
-        _id: {
-          $oid: "65f271e2b77b2abe952fc7fb",
-        },
-        name: "USER",
-        permissions: ["processor"],
-      },
-    ],
-    createdBy: {
-      $date: "2024-03-14T03:41:22.242Z",
-    },
-    __v: 0,
-  },
-  {
-    id: "6618cc6f5e92011cb7439946",
-    sub: "3c638e3c-970f-4864-bab4-b2d7647ce6de",
-    email: "clint.llosala@aretex.com.au",
-    name: "Clint Joey Llosala",
-    picture:
-      "https://lh3.googleusercontent.com/a/ACg8ocLdBgl8CvLscUNJeEcaTnSZTELeDY4YyUV2hvoH151V6BBaRQ=s96-c",
-    hasOnboardingData: false,
-    role: [
-      {
-        _id: {
-          $oid: "6618cc6f5e92011cb7439947",
-        },
-        name: "USER",
-        permissions: ["processor"],
-      },
-    ],
-    createdBy: {
-      $date: "2024-04-12T05:53:51.517Z",
-    },
-    __v: 0,
-  },
-  {
-    id: "663aca918e63145854125576",
-    sub: "acbd0f62-4ddf-43a1-b2a2-fdacbae5d3ed",
-    email: "gerome.blanco@aretex.com.au",
-    name: "Gerome Blanco",
-    picture:
-      "https://lh3.googleusercontent.com/a/ACg8ocK-oRnfsN1iyKaShecdglBcr73YxLmy8enJquYyQiZ-caA4Qws=s96-c",
-    hasOnboardingData: false,
-    role: [
-      {
-        _id: {
-          $oid: "663aca918e63145854125577",
-        },
-        name: "USER",
-        permissions: ["processor"],
-      },
-    ],
-    createdBy: {
-      $date: "2024-05-08T00:42:57.150Z",
-    },
-    __v: 0,
-  },
-  {
-    id: "6678c9253a865f4d1646149c",
-    sub: "b63b2925-152e-4dcc-a765-c4a786cb2ea2",
-    email: "andre.yu@aretex.com.au",
-    name: "Andrei Emmanuel Yu",
-    picture:
-      "https://lh3.googleusercontent.com/a/ACg8ocIErtDD5EfwTX2HCnh0k-VpHcQmaxKSkv_mU7I9d4rgaKTu12Q=s96-c",
-    hasOnboardingData: false,
-    role: [
-      {
-        _id: {
-          $oid: "65eec076cbd659d0869a2bcb",
-        },
-        name: "USER",
-        permissions: ["processor"],
-      },
-    ],
-    createdBy: {
-      $date: "2024-03-11T08:27:34.227Z",
-    },
-    __v: 0,
-  },
+export const userRolesListAtom = atom([]);
+
+export const fetchUserRolesListAtom = atom(null, async (get, set, sub) => {
+  const list = await restread("/user/tagged");
+  if (list.success) {
+    const convertedRoles = list.result.map((user) => {
+      return {
+        key: user.sub,
+        role: user.role,
+        name: user.name,
+        picture: user.picture,
+        email: user.email,
+        sub: user.sub,
+        _id: user._id,
+      };
+    });
+
+    set(userRolesListAtom, convertedRoles);
+    return convertedRoles;
+  } else {
+    return [];
+  }
+});
+
+export const updateUserRolesAtom = atom(null, async (get, set, update) => {
+  const { updatedRoles } = update;
+  const user = await get(userAtom);
+
+  try {
+    const updatedUser = await restupdate(`/user/update-role`, {
+      sub: user.sub,
+      role: updatedRoles,
+    });
+
+    set(fetchUserRolesListAtom, {});
+    return updatedUser;
+  } catch (error) {
+    console.error("Error while updating user roles:", error);
+    return {};
+  }
+});
+
+export const selectedRolesAtom = atom(new Set([]));
+
+export const selectedRoleTabAtom = atom("all");
+export const roleTabsAtom = atom([
+  { key: "all", title: "All" },
+  { key: "admin", title: "Admin" },
+  { key: "tl", title: "Team Lead" },
+  { key: "hr", title: "Human Resource" },
+  { key: "user", title: "User" },
+]);
+
+export const roleSelectionAtom = atom([
+  { key: "user", title: "User" },
+  { key: "admin", title: "Admin" },
+  { key: "tl", title: "Team Lead" },
+  { key: "hr", title: "Human Resource" },
 ]);
