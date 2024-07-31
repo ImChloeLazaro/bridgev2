2e0505f07855d056c38a8af64512790071d89254
cookiejarr1 <jarodlayugan@gmail.com>
2024-07-31 12:16:26 +1000

    temp commit dont pull

--- app/admin/roles/page.jsx
+++ app/admin/roles/page.jsx
@@ -2,14 +2,14 @@
 import { userAtom } from "@/app/store/UserStore";
 import { useRoles } from "@/app/utils/roles";
 import { useAtomValue } from "jotai";
-import UserRolesList from "./components/UserRolesList";
+
 
 const UserRoles = () => {
   const users = useAtomValue(userAtom).role;
   const roles = useRoles(users);
   return (
     <div className="flex justify-center items-center w-full h-full p-0 lg:p-6 ">
-      <UserRolesList />
+
     </div>
   );
 };
